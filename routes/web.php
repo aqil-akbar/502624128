373a16672d550157712eb4088dfc08ab4e4695f4
Aqil Akbar <aqilakbar1106@gmail.com>
2026-05-25 20:18:18 +0700

    Pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
+
+//pertemuan 9
+Route::get('/pegawainama/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+//route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+// Pertemuan 11
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
