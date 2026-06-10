1e8f6d8fd3bfab54bdd414455ded16cf38295709
Aqil Akbar <aqilakbar1106@gmail.com>
2026-06-10 21:46:05 +0700

    pertemuan13

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,26 @@ use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\PegawaiDBController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//pertemuan 7
+Route::get('/halo', function () {
+	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <b>www.malasngoding.com</b>";
+});
+
+Route::get('/blog', function () {
+	return view('blog');
+});
+
+Route::get('/DELL', function () {
+	return view('dell');
+});
+
+Route::get('/dosen', [DosenController::class, 'index']);
+Route::get('/biodata', [DosenController::class, 'biodata']);
+
 //pertemuan 9
 Route::get('/pegawainama/{nama}', [PegawaiController::class, 'index']);
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
@@ -15,27 +35,61 @@ Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
 
+//tugas pertemuan 7
+Route::get('/tugas', function () {
+    return view('tugas.menu', [
+        'nama' => 'Ahmad \'Aaqila Akbar'
+    ]);
+});
+
+Route::get('/tugas/pertemuan-1', function () {
+    return view('tugas.intro');
+});
+
+Route::get('/tugas/pertemuan-2', function () {
+    return view('tugas.news');
+});
+
+Route::get('/tugas/pertemuan-3', function () {
+    return view('tugas.responsive');
+});
+
+Route::get('/tugas/tugas-pertemuan-3', function () {
+    return view('tugas.tugas-boostrap4');
+});
+
+Route::get('/tugas/pertemuan-4', function () {
+    return view('tugas.layout');
+});
+
+Route::get('/tugas/pertemuan-5', function () {
+    return view('tugas.dell2');
+});
+
+Route::get('/tugas/tugas-pertemuan-5', function () {
+    return view('tugas.linktree');
+});
+
+Route::get('/tugas/pertemuan-5.1', function () {
+    return view('tugas.index');
+});
+
+Route::get('/tugas/pertemuan-3.1', function () {
+    return view('tugas.template');
+});
+
+Route::get('/tugas/pertemuan-2.1', function () {
+    return view('tugas.news-1');
+});
+
+//  ROUTE CRUD
 // Pertemuan 11
 Route::get('/pegawai', [PegawaiDBController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('halo', function () {
-    return "<hi1>halo, selamat datang</h1> di tutorial laravel <b>www.malasngoding.com </b>";
-});
-
-Route::get('blog', function () {
-	return view('blog');
-});
-
-Route::get('pert5', function () {
-	return view('pertemuan5');
-});
-
-Route::get('dosen', [DosenController::class, 'index'
-]);
-
-Route::get('biodata', [DosenController::class, 'biodata'
-]);
+// Pertemuan 13
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
